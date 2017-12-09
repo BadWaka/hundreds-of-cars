7c8c5d45049f7dde45c7f2f59e5c0759954a8cc3
BadWaka <1456683844@qq.com>
2017-12-09 17:00:12 +0800

    登录注册框

--- src/containers/LoginModal/LoginModal.vue
+++ src/containers/LoginModal/LoginModal.vue
@@ -1,15 +1,21 @@
 <template>
   <modal @close="handleClose">
     <section class="login-modal">
-      <div class="title c-font-20">
+      <div class="title c-font-big">
         <div
-          class="tab-sign-in c-btn selected"
+          class="tab-sign-in c-btn "
+          :class="{
+            'selected':currentTab === 0
+          }"
           @click="handleTabChange(0)">
           登录
         </div>
         <div class="divider c-gap-left-small c-gap-right-small">|</div>
         <div
           class="tab-sign-up c-btn"
+          :class="{
+            'selected':currentTab === 1
+          }"
           @click="handleTabChange(1)">
           注册
         </div>
@@ -17,13 +23,13 @@
 
       <section class="bottom sign-in">
         <!--账号-->
-        <section>
+        <section class="form-item">
           <div class="c-font-big">电子邮箱 / 手机号码</div>
           <el-input
             class="c-gap-top"
             v-model="form.username"></el-input>
           <div
-            v-if="tabStatus === 'signIn'"
+            v-if="currentTab === 0"
             class="row-right c-gap-top">
             <span class="link c-btn">
               忘记用户名？
@@ -31,13 +37,13 @@
           </div>
         </section>
         <!--密码-->
-        <section class="">
+        <section class="form-item c-gap-top-20">
           <div class="c-font-big">密码</div>
           <el-input
             class="c-gap-top"
             v-model="form.password"></el-input>
           <div
-            v-if="tabStatus === 'signIn'"
+            v-if="currentTab === 0"
             class="row-right c-gap-top">
             <span class="link c-btn">
               忘记密码？
@@ -45,7 +51,7 @@
           </div>
         </section>
         <!--按钮-->
-        <el-button class="c-gap-top-20 btn-sign-in" type="success">登录</el-button>
+        <el-button class="btn-sign-in" type="success">登录</el-button>
       </section>
 
     </section>
@@ -59,7 +65,7 @@
     name: 'FinancialCalculatorModal',
     data() {
       return {
-        tabStatus: 'signIn',  // 当前状态；登录还是注册
+        currentTab: 0,  // 当前状态；登录还是注册
         form: {
           username: '',
           password: '',
@@ -75,6 +81,7 @@
       },
       handleTabChange(index) {
         console.log(index);
+        this.currentTab = index;
       }
     }
   }
@@ -107,16 +114,24 @@
     .bottom {
       margin-top: 60px;
 
-      .btn-sign-in {
-        width: 100%;
+      .form-item {
+        position: relative;
+
+        .row-right {
+          position: absolute;
+          bottom: -30px;
+          right: 0;
+          text-align: right;
+
+          .link {
+            color: $lightBlue500;
+          }
+        }
       }
 
-      .row-right {
-        text-align: right;
-
-        .link {
-          color: $lightBlue500;
-        }
+      .btn-sign-in {
+        margin-top: 60px;
+        width: 100%;
       }
     }
   }
