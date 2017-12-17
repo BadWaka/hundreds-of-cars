854c61f495232609549890b9e3fa2ed761d48947
BadWaka <1456683844@qq.com>
2017-12-17 10:41:51 +0800

    收藏

--- src/containers/PersonInfo/Collection/Collection.vue
+++ src/containers/PersonInfo/Collection/Collection.vue
@@ -1,11 +1,17 @@
 <template>
   <section class="collection">
     <div
-      class="c-font-20">
+      class="c-font-20 c-gap-top-20">
       <i class="el-icon-star-on"></i>
       <span class="c-gap-left">收藏的车</span>
     </div>
-    <collection-item></collection-item>
+    <section class="c-flex collections c-gap-top">
+      <collection-item
+        v-for="(item, index) in collections"
+        :key="index"
+        :data="item">
+      </collection-item>
+    </section>
   </section>
 </template>
 
@@ -15,7 +21,30 @@
   export default {
     name: 'Collection',
     data() {
-      return {}
+      return {
+        collections: [
+          {
+            imgSrc: 'http://img.hb.aicdn.com/0435793935078f948ad3edc0e24b6c05a8c1b260320b1-TE5bwL_fw658',
+            title: '新款 2016 一汽-大众 CC 1.8T',
+            price: '17.70万起',
+            mileage: '3000公里',  // 里程
+            carColor: '白色', // 车身颜色
+            insideColor: '沙色/黑色', // 内饰颜色
+            fuelConsumption: '7.5L', // 油耗
+            carBody: '三厢',  // 车体
+          },
+          {
+            imgSrc: 'http://img.hb.aicdn.com/1ed9f3193dcd77b0635f5fff96a656a13ca1670e85615-wztlF2_fw658',
+            title: '新款 2016 一汽-大众 CC 2T',
+            price: '25.70万起',
+            mileage: '1500公里',  // 里程
+            carColor: '白色', // 车身颜色
+            insideColor: '沙色/黑色', // 内饰颜色
+            fuelConsumption: '7.5L', // 油耗
+            carBody: '三厢',  // 车体
+          }
+        ]
+      }
     },
     components: {
       CollectionItem
@@ -30,5 +59,13 @@
 
   @import "../../../assets/app";
 
+  .el-icon-star-on {
+    color: #E31594;
+  }
+
+  .collections {
+    justify-content: space-between;
+    padding: 0 40px;
+  }
 
 </style>
--- src/containers/PersonInfo/Collection/CollectionItem/CollectionItem.vue
+++ src/containers/PersonInfo/Collection/CollectionItem/CollectionItem.vue
@@ -1,48 +1,57 @@
 <template>
   <section class="collection-item">
-    <el-checkbox>对比</el-checkbox>
+    <div class="row1">
+      <el-checkbox>对比</el-checkbox>
+    </div>
     <div
-      class="img"
+      class="img c-gap-top"
       :style="{
         'background-image': 'url(' + data.imgSrc + ')'
       }">
     </div>
-    <div>{{data.title}}</div>
-    <div>{{data.price}}</div>
+    <div class="c-gap-top-20">{{data.title}}</div>
+    <div class="c-font-20">{{data.price}}</div>
 
-    <div>
-      <span class="c-color-grey">里程数：</span>{{data.mileage}}
-    </div>
-    <div>
-      <span class="c-color-grey">车身颜色：</span>{{data.carColor}}
-    </div>
-    <div>
-      <span class="c-color-grey">内饰颜色：</span>{{data.insideColor}}
-    </div>
-    <div>
-      <span class="c-color-grey">油耗：</span>{{data.fuelConsumption}}
-    </div>
-    <div>
-      <span class="c-color-grey">车体：</span>{{data.carBody}}
-    </div>
+    <section class="bottom">
+      <section class="left">
+        <div>
+          <span class="c-color-grey">里程数：</span>{{data.mileage}}
+        </div>
+        <div>
+          <span class="c-color-grey">车身颜色：</span>{{data.carColor}}
+        </div>
+        <div>
+          <span class="c-color-grey">油耗：</span>{{data.fuelConsumption}}
+        </div>
+      </section>
+      <section class="right">
+        <div>
+          <span class="c-color-grey">内饰颜色：</span>{{data.insideColor}}
+        </div>
+        <div>
+          <span class="c-color-grey">车体：</span>{{data.carBody}}
+        </div>
+      </section>
+    </section>
   </section>
 </template>
 
 <script>
   export default {
     name: 'CollectionItem',
+    props: ['data'],
     data() {
       return {
-        data: {
-          imgSrc: 'http://img.hb.aicdn.com/0435793935078f948ad3edc0e24b6c05a8c1b260320b1-TE5bwL_fw658',
-          title: '新款 2016 一汽-大众 CC 1.8T',
-          price: '17.70万起',
-          mileage: '3000公里',  // 里程
-          carColor: '白色', // 车身颜色
-          insideColor: '沙色/黑色', // 内饰颜色
-          fuelConsumption: '7.5L', // 油耗
-          carBody: '三厢',  // 车体
-        }
+        // data: {
+        //   imgSrc: 'http://img.hb.aicdn.com/0435793935078f948ad3edc0e24b6c05a8c1b260320b1-TE5bwL_fw658',
+        //   title: '新款 2016 一汽-大众 CC 1.8T',
+        //   price: '17.70万起',
+        //   mileage: '3000公里',  // 里程
+        //   carColor: '白色', // 车身颜色
+        //   insideColor: '沙色/黑色', // 内饰颜色
+        //   fuelConsumption: '7.5L', // 油耗
+        //   carBody: '三厢',  // 车体
+        // }
       }
     },
     components: {},
@@ -57,12 +66,26 @@
   @import "../../../../assets/app";
 
   .collection-item {
+    width: 400px;
+
+    .row1 {
+      text-align: right;
+    }
 
     .img {
       width: 400px;
       height: 240px;
       background: no-repeat center;
       background-size: cover;
+    }
+
+    .bottom {
+      display: flex;
+      margin-top: 40px;
+
+      .right {
+        margin-left: 60px;
+      }
     }
   }
 
