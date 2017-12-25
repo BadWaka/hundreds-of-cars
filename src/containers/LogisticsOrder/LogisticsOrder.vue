54f44432de8a33a7130ba44401fbc4195fb9bdda
BadWaka <1456683844@qq.com>
2017-12-25 21:14:58 +0800

    bak

--- src/containers/LogisticsOrder/LogisticsOrder.vue
+++ src/containers/LogisticsOrder/LogisticsOrder.vue
@@ -1,7 +1,27 @@
 <template>
   <section class="logistics-order c-gap-inner-header c-container">
-    <div>物流订单模板</div>
-
+    <!--物流订单模板-->
+    <div class="c-gap-top-20 c-font-20">物流订单模板</div>
+    <section class="c-gap-top-20">
+      <el-upload
+        action="https://jsonplaceholder.typicode.com/posts/"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+    </section>
+    <!--第二行-->
+    <section class="c-gap-top-20 c-flex row2">
+      <!--已保存模板-->
+      <section class="row-item">
+        <div class="c-font-20">已保存模板</div>
+      </section>
+      <!--已接收报价-->
+      <section class="row-item">
+        <div class="c-font-20">已接收报价</div>
+      </section>
+    </section>
   </section>
 </template>
 
@@ -12,7 +32,14 @@
       return {}
     },
     components: {},
-    methods: {},
+    methods: {
+      handlePictureCardPreview() {
+        console.log('handlePictureCardPreview');
+      },
+      handleRemove() {
+        console.log('handleRemove');
+      }
+    },
     mounted() {
     }
   }
@@ -23,6 +50,13 @@
   @import "../../assets/app";
 
   .logistics-order {
+
+    .row2 {
+
+      .row-item {
+        flex: 1;
+      }
+    }
 
   }
 
